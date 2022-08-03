353e020cc09cb25a59744a803be4f0aacf1deb38
mizuki <mizuki2718@ruri.waseda.jp>
2022-08-03 14:19:15 +0900

    make directory

--- fullspace_run.py
+++ fullspace_run.py
@@ -19,6 +19,7 @@ import time
 import os
 import pickle
 import pathlib
+from pathlib import Path
 from absl import app
 from absl import flags
 from matplotlib import animation
@@ -43,61 +44,21 @@ data_dir = os.path.join(root_dir, 'data', dataset_name)
 fullspace_data_path = os.path.join(data_dir, 'fullspace_traj.pkl')
 pca_base_path = os.path.join(data_dir, 'pca_base.pkl')
 
-def main(unused_argv):
-    print('fullspace rollout starting...')
+def prepare_files_and_directories():
+    # make all the necessary directories
+    Path(rollout_dir).mkdir(parents=True, exist_ok=True)
+    Path(data_dir).mkdir(parents=True, exist_ok=True)
 
-    fig = plt.figure(figsize=(19.2, 10.8))
-    ax = fig.add_subplot(111, projection='3d')
-    skip = 1
-    num_steps = 500
-    num_frames = num_steps
-    num = 0
-
-    # Setup solvers
-    models = []
-
-    # flag
-    res_w = 50
-    res_h = 30
-    len_w = 0.5
-    len_h = 0.3
-    models.append(geometry_init.generate_plane(res_w, res_h, len_w, len_h))
-
-    for _ in range(num_steps):
-        step = (num * skip)
-        num += 1
-
-        ax.cla()
-
-        ax.set_xlim([-1.0, 1.0])
-        ax.set_ylim([-1.0, 1.0])
-        ax.set_zlim([-1.0, 1.0])
-
-        for model in models:
-            vert = model.rendering_verts
-            faces = model.rendering_faces
-            ax.plot_trisurf(vert[:, 0], vert[:, 1],
-                            faces, vert[:, 2], shade=True)
-
-        ax.set_title('Step %d' % (step))
-        print('Step: %d' % (step))
-
-        # advance time
-        for model in models:
-            model.simulate()
-        
-        fig_name = 'fullspace_traj' + str(step) + '.jpg'
-        plt.savefig(os.path.join(rollout_dir, fig_name))
-        plt.show(block=True)
 
 # def main(unused_argv):
 #     print('fullspace rollout starting...')
 
 #     fig = plt.figure(figsize=(19.2, 10.8))
 #     ax = fig.add_subplot(111, projection='3d')
-#     skip = 10
+#     skip = 1
 #     num_steps = 500
 #     num_frames = num_steps
+#     num = 0
 
 #     # Setup solvers
 #     models = []
@@ -109,8 +70,9 @@ def main(unused_argv):
 #     len_h = 0.3
 #     models.append(geometry_init.generate_plane(res_w, res_h, len_w, len_h))
 
-#     def animate(num):
-#         step = (num * skip) % num_steps
+#     for _ in range(num_steps):
+#         step = (num * skip)
+#         num += 1
 
 #         ax.cla()
 
@@ -126,23 +88,68 @@ def main(unused_argv):
 
 #         ax.set_title('Step %d' % (step))
 #         print('Step: %d' % (step))
-#         print(time.time())
 
 #         # advance time
 #         for model in models:
 #             model.simulate()
+        
+#         fig_name = 'fullspace_traj' + str(step) + '.jpg'
+#         plt.savefig(os.path.join(rollout_dir, fig_name))
+#         plt.show(block=True)
 
-#         return fig,
+def main(unused_argv):
+    print('fullspace rollout starting...')
 
-#     # ani = animation.FuncAnimation(fig, animate, frames=math.floor(num_frames * 0.1), interval=100)
-#     # ani = animation.FuncAnimation(fig, animate, interval=100)
-#     # ani = animation.FuncAnimation(
-#     #     fig, animate, frames=num_frames * 100, interval=50)
-#     ani = animation.FuncAnimation(
-#         fig, animate, frames=num_frames * 100, interval=50)
+    fig = plt.figure(figsize=(19.2, 10.8))
+    ax = fig.add_subplot(111, projection='3d')
+    skip = 10
+    num_steps = 500
+    num_frames = num_steps
 
-#     ani.save(os.path.join(rollout_dir, 'fullspace_traj.mp4'), writer="ffmpeg")
-#     plt.show(block=True)
+    # Setup solvers
+    models = []
+
+    # flag
+    res_w = 50
+    res_h = 30
+    len_w = 0.5
+    len_h = 0.3
+    models.append(geometry_init.generate_plane(res_w, res_h, len_w, len_h))
+
+    def animate(num):
+        step = (num * skip) % num_steps
+
+        ax.cla()
+
+        ax.set_xlim([-1.0, 1.0])
+        ax.set_ylim([-1.0, 1.0])
+        ax.set_zlim([-1.0, 1.0])
+
+        for model in models:
+            vert = model.rendering_verts
+            faces = model.rendering_faces
+            ax.plot_trisurf(vert[:, 0], vert[:, 1],
+                            faces, vert[:, 2], shade=True)
+
+        ax.set_title('Step %d' % (step))
+        print('Step: %d' % (step))
+        print(time.time())
+
+        # advance time
+        for model in models:
+            model.simulate()
+
+        return fig,
+
+    # ani = animation.FuncAnimation(fig, animate, frames=math.floor(num_frames * 0.1), interval=100)
+    # ani = animation.FuncAnimation(fig, animate, interval=100)
+    # ani = animation.FuncAnimation(
+    #     fig, animate, frames=num_frames * 100, interval=50)
+    ani = animation.FuncAnimation(
+        fig, animate, frames=num_frames * 100, interval=50)
+
+    ani.save(os.path.join(rollout_dir, 'fullspace_traj.mp4'), writer="ffmpeg")
+    plt.show(block=True)
 
 
 if __name__ == '__main__':
