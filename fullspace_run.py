0eb80725df98150ddc9a1a5b39e1e9d9114d5ebd
mizuki <mizuki2718@ruri.waseda.jp>
2022-08-04 18:42:31 +0900

    save pca base

--- fullspace_run.py
+++ fullspace_run.py
@@ -14,9 +14,11 @@ import numpy as np
 import math
 import pd_model
 import geometry_init
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 #
-dataset_name = 'flag'
+dataset_name = 'flag_test'
 rollout_name = '1'
 
 #
@@ -27,13 +29,46 @@ rollout_dir = os.path.join(output_dir, 'rollout')
 data_dir = os.path.join(root_dir, 'data', dataset_name)
 
 #
-fullspace_data_path = os.path.join(data_dir, 'fullspace_traj.pkl')
-pca_base_path = os.path.join(data_dir, 'pca_base.pkl')
+fullspace_data_path = os.path.join(data_dir, 'fullspace_traj.npz')
+pca_base_path = os.path.join(data_dir, 'pca_base.npz')
+
+'''Paramters'''
+pca_dim = 50
 
 def prepare_files_and_directories():
     # make all the necessary directories
     Path(rollout_dir).mkdir(parents=True, exist_ok=True)
     Path(data_dir).mkdir(parents=True, exist_ok=True)
+
+def construct_base(traj, n):
+    scaler = StandardScaler(with_std=False)
+    pos_scaled = scaler.fit_transform(traj)
+
+    # TODO: Whiten?
+    # Whitening doesnt seem to matter to the result in terms of dimensionally reduction
+    pca_pos = PCA(n_components=n, whiten=False)
+    pca_pos.fit(pos_scaled)
+
+    # Show Explained variance ratio
+    plt.figure()
+    plt.plot(np.hstack([0, pca_pos.explained_variance_ratio_]).cumsum(), 'o-')
+    plt.xticks(range(5))
+    plt.xlabel('Components')
+    plt.ylabel('Explained variance ratio')
+    plt.grid()
+    plt.savefig(os.path.join(data_dir, 'posExpVarRatio.png'))
+
+    # projection to PC1 PC2 space
+    feature_pos = pca_pos.transform(traj)
+    # 
+    plt.figure(figsize=(6, 6))
+    plt.scatter(feature_pos[:, 0], feature_pos[:, 1], alpha=0.8)
+    plt.grid()
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.savefig(os.path.join(data_dir, 'fullspace_traj_projection.png'))
+
+    return pca_pos
 
 def main(unused_argv):
     #
@@ -82,12 +117,20 @@ def main(unused_argv):
         print(time.time() - start)
         print(models[0].rendering_verts[0,:])
 
+        # save positions
+        fullspace_traj[num,:] = model.position.T
+
         # advance time
         for _ in range(skip):
             for model in models:
                 model.simulate()
-
         return fig,
+
+    pca_rslt = construct_base(fullspace_traj, pca_dim)
+    components = pca_rslt.components_
+    mean = np.mean(fullspace_traj, axis=0)
+
+    np.savez(os.path.join(data_dir, 'pca_base.npz'), base=components, mean=mean)
 
     # ani = animation.FuncAnimation(fig, animate, frames=math.floor(num_frames * 0.1), interval=100)
     # ani = animation.FuncAnimation(fig, animate, interval=100)
