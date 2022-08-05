25d11f721745803914cf89887b07e94af3db778a
mizuki <mizuki2718@ruri.waseda.jp>
2022-08-05 17:20:59 +0900

    small

--- geometry_init.py
+++ geometry_init.py
@@ -56,7 +56,7 @@ def generate_plane(width, height, MAX_WIDTH_SIZE=0.5, MAX_HEIGHT_SIZE=0.3):
 
     fixed_points.append(0)
     fixed_points.append(bottom_left)
-    return pd_model.PDModel(verts, faces, uvs, constraints=constraints)
+    return pd_model.PDModel(verts, faces, uvs, constraints=constraints, fixed_points=fixed_points)
 
 
 def add_face(v_1, v_2, v_3, faces):
